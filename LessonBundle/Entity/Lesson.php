dfd16e14a4602e392ecadf467fc7414a0be2138d
Stéphane Klein <stephaneklein221@gmail.com>
2016-03-19 18:01:11 +0100

    Add LessonBundle package

---
+++ LessonBundle/Entity/Lesson.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: gaetan
+ * Date: 26/06/13
+ * Time: 15:52
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Icap\LessonBundle\Entity;
+
+use Claroline\CoreBundle\Entity\Resource\AbstractResource;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="icap__lesson")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Lesson extends AbstractResource
+{
+    /**
+     * @ORM\OneToOne(targetEntity="Icap\LessonBundle\Entity\Chapter", cascade={"all"})
+     * @ORM\JoinColumn(name="root_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    private $root;
+
+    /**
+     * @param mixed $root
+     */
+    public function setRoot($root)
+    {
+        $this->root = $root;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoot()
+    {
+        return $this->root;
+    }
+
+    /**
+     * @ORM\PostPersist
+     */
+    public function createRoot(LifecycleEventArgs $event){
+        if($this->getRoot() == null){
+            $em = $event->getEntityManager();
+            $rootLesson = $this->getRoot();
+            if($rootLesson == null)
+            {
+                $rootLesson = new Chapter();
+                $rootLesson->setLesson($this);
+                $rootLesson->setTitle("root_".$this->getId());
+                $this->setRoot($rootLesson);
+
+                $em->getRepository('IcapLessonBundle:Chapter')->persistAsFirstChild($rootLesson);
+                $em->flush();
+            }
+        }
+    }
+}
---
+++ LessonBundle/Repository/ChapterRepository.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Nicolas
+ * Date: 18/10/13
+ * Time: 09:36
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Icap\LessonBundle\Repository;
+
+use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
+use Icap\LessonBundle\Entity\Chapter;
+use Icap\LessonBundle\Entity\Lesson;
+
+class ChapterRepository extends NestedTreeRepository{
+
+    function getFirstChapter(Lesson $lesson){
+        return $this->findOneBy(array('lesson' => $lesson, 'root' => $lesson->getRoot()->getId(), 'left' => 2));
+    }
+
+    function getChapterTree(Chapter $chapter){
+        return $this->childrenHierarchy($chapter, false, array(), true);
+    }
+
+    /**
+     * @param Chapter $chapter
+     * @return Tree $tree
+     */
+    public function buildChapterTree(Chapter $chapter)
+    {
+        $queryBuilder = $this->createQueryBuilder('chapter')
+            ->select('chapter')
+            ->andWhere('chapter.root = :rootId')
+            ->orderBy('chapter.root, chapter.left', 'ASC')
+            ->setParameter('rootId', $chapter->getId());
+        $options = array('decorate' => false);
+        $tree = $this->buildTree($queryBuilder->getQuery()->getArrayResult(), $options);
+
+        return $tree;
+    }
+
+    function getChapterAndChapterChildren(Chapter $chapter){
+        return $this->children($chapter, false, null, 'ASC', true);
+    }
+
+    function getChapterChildren(Chapter $chapter){
+        return $this->children($chapter, false, null, 'ASC', false);
+    }
+
+    function getChapterFirstChild(Chapter $chapter){
+        try {
+            return $this->childrenQueryBuilder($chapter)
+                ->setFirstResult(0)
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getSingleResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            return null;
+        }
+    }
+
+    function getChapterAndDirectChapterChildren(Chapter $chapter){
+        return $this->children($chapter, true, null, 'ASC', true);
+    }
+
+    function getDirectChapterChildren(Chapter $chapter){
+        return $this->children($chapter, true, null, 'ASC', false);
+    }
+
+    function getNextSibling(Chapter $chapter){
+        try{
+            return $this->getNextSiblingsQueryBuilder($chapter)
+                ->setFirstResult(0)
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getSingleResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            return null;
+        }
+    }
+
+
+
+    function getPreviousSibling(Chapter $chapter){
+        try{
+            return $this->getPrevSiblingsQueryBuilder($chapter)
+                ->setFirstResult(0)
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getSingleResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            return null;
+        }
+    }
+
+    function getNextChapter(Chapter $chapter){
+        try{
+            $qb = $this->_em->createQueryBuilder();
+            return $this->_em->createQueryBuilder()->add('select', 'c')
+                ->add('from', 'Icap\LessonBundle\Entity\Chapter c')
+                ->innerJoin('c.lesson',' l')
+                ->where($qb->expr()->andx(
+                    $qb->expr()->gt('c.left', '?1'),
+                    $qb->expr()->eq('l.id', '?2')
+                ))
+                ->orderBy("c.left", 'ASC')
+                ->setParameter(1, $chapter->getLeft())
+                ->setParameter(2, $chapter->getLesson())
+                ->setFirstResult(0)
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getSingleResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            return null;
+        }
+    }
+
+
+    function getPreviousChapter($chapter){
+        try{
+            $qb = $this->_em->createQueryBuilder();
+            return $this->_em->createQueryBuilder()->add('select', 'c')
+                ->add('from', 'Icap\LessonBundle\Entity\Chapter c')
+                ->innerJoin('c.lesson',' l')
+                ->where($qb->expr()->andx(
+                    $qb->expr()->lt('c.left', '?1'),
+                    $qb->expr()->eq('l.id', '?2'),
+                    $qb->expr()->not($qb->expr()->eq('c.id', '?3'))
+                ))
+                ->orderBy("c.left", 'DESC')
+                ->setParameter(1, $chapter->getLeft())
+                ->setParameter(2, $chapter->getLesson()->getId())
+                ->setParameter(3, $chapter->getLesson()->getRoot()->getId())
+                ->setFirstResult(0)
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getSingleResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            return null;
+        }
+    }
+
+    function getChapterById($chapterId, $lessonId){
+        try{
+            $qb = $this->_em->createQueryBuilder();
+            return $this->_em->createQueryBuilder()->add('select', 'c')
+                ->add('from', 'Icap\LessonBundle\Entity\Chapter c')
+                ->innerJoin('c.lesson',' l')
+                ->where($qb->expr()->andx(
+                    $qb->expr()->eq('c.id', '?1'),
+                    $qb->expr()->eq('l.id', '?2')
+                ))
+                ->setParameter(1, $chapterId)
+                ->setParameter(2, $lessonId)
+                ->getQuery()
+                ->getSingleResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            return null;
+        }
+    }
+
+    function getChapterBySLug($chapterSlug, $lessonId){
+        try{
+            $qb = $this->_em->createQueryBuilder();
+            return $this->_em->createQueryBuilder()->add('select', 'c')
+                ->add('from', 'Icap\LessonBundle\Entity\Chapter c')
+                ->innerJoin('c.lesson',' l')
+                ->where($qb->expr()->andx(
+                    $qb->expr()->eq('c.slug', '?1'),
+                    $qb->expr()->eq('l.id', '?2')
+                ))
+                ->setParameter(1, $chapterSlug)
+                ->setParameter(2, $lessonId)
+                ->getQuery()
+                ->getSingleResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            return null;
+        }
+    }
+}
